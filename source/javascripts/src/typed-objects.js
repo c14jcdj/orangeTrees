681a19be0701c464bda8cc04f3ee4e02518842ab
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-05 13:54:17 -0700

    passing tests for createTree()

--- source/javascripts/src/typed-objects.js
+++ source/javascripts/src/typed-objects.js
@@ -1,1 +1,9 @@
-// Write your Orange Tree code here - you may use constructor functions 
+function createTree() {
+    return new Tree
+}
+
+function Tree() {
+    this.age = 0;
+    this.height = 0;
+    this.orangeCount = 0;
+}
