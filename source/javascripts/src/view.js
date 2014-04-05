84c7ccf15590160dc997c916acf4dc29cd3c9f84
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-05 16:06:01 -0700

    refactored to oo js

--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+    tController = new TreeController();
+    tView = new TreeView();
+    tController.run();
+
+
+});
+
+function TreeView() {}
+
+function TreeController() {}
+
+TreeController.prototype.run = function() {
     $('.plant').on('click', function() {
         var tree = new Tree();
+        tView.addGrove();
+        tView.age(tree);
+        tView.pick(tree);
+    })
+}
+
+TreeView.prototype = {
+    addGrove: function() {
+
         $('#orange-tree-template').removeClass();
         this.disabled = true;
+    },
+
+
+    age: function(tree) {
         $('button.age').on('click', function() {
             tree.grow();
             $('p.age').text(tree.age);
@@ -13,9 +38,17 @@ $(document).ready(function() {
                 }
             }
         })
+    },
+
+    pick: function(tree) {
         $('button.pick').on('click', function() {
-            $('p.fruit-count').text(tree.orangeCountTotal -= 1);
+            if (tree.orangeCountTotal == 0) {
+                fC = 'No more oranges';
+            } else {
+                fC = tree.orangeCountTotal -= 1;
+            }
+            $('p.fruit-count').text(fC);
             $('.display-tree-big img:last').remove();
         })
-    })
-});
+    }
+}
