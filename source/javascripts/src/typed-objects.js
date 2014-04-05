a8ff0ebc9cc3ee13ec12a37a6ced94fe21100cb5
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-05 15:38:17 -0700

    working orange grove

--- source/javascripts/src/typed-objects.js
+++ source/javascripts/src/typed-objects.js
@@ -6,6 +6,7 @@ function Tree() {
     this.age = 0;
     this.height = 0;
     this.orangeCount = 0;
+    this.orangeCountTotal = 0;
     FRUIT_BEARING_AGE = 2;
     MAX_AGE = 8;
     this.isAlive = true
@@ -24,7 +25,8 @@ Tree.prototype = {
         this.age += 1;
         this.height += 10;
         if (this.age >= FRUIT_BEARING_AGE) {
-            this.orangeCount = Math.random() * 5;
+            this.orangeCount = Math.floor(Math.random() * 3 + 1);
+            this.orangeCountTotal += this.orangeCount
         };
         if (this.age > MAX_AGE) {
             this.isAlive = false;
--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -1,1 +1,21 @@
-/* Write your JS to modify the view here */
+$(document).ready(function() {
+    $('.plant').on('click', function() {
+        var tree = new Tree();
+        $('#orange-tree-template').removeClass();
+        this.disabled = true;
+        $('button.age').on('click', function() {
+            tree.grow();
+            $('p.age').text(tree.age);
+            if (tree.age >= FRUIT_BEARING_AGE) {
+                for (var i = 0; i < tree.orangeCount; i++) {
+                    $('.display-tree-big').append("<img src='images/orange.gif '>")
+                    $('p.fruit-count').text(tree.orangeCountTotal)
+                }
+            }
+        })
+        $('button.pick').on('click', function() {
+            $('p.fruit-count').text(tree.orangeCountTotal -= 1);
+            $('.display-tree-big img:last').remove();
+        })
+    })
+});
